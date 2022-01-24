d0d5e272082a3dd6ca6abea7403f8ed95aab9cad
root <root@ip-172-31-29-136.us-west-2.compute.internal>
2022-01-24 19:39:44 +0000

    test

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/AsifEnglender/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,9 +12,9 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('yanivomc/amdocsapp')
+            repositoryName('asifeng/newjenkinstest')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('yaniv-dockerhub')
+            registryCredentials('8d2f03eb-cc76-4a1a-b749-6cfd5caa566c')
             buildContext('./basics/')
             forcePull(false)
             forceTag(false)
